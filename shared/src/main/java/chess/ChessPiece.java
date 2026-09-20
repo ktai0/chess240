f13783da0e5ceea33c7a2f408dd206fb0ee4e2a3
Keilyn Tai <keilyntoma1@gmail.com>
2026-09-19 23:32:23 -0600

    added equals and hashcode functions

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -56,9 +56,7 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece= board.getPiece(myPosition);
         if (piece.getPieceType() == PieceType.BISHOP) {
-            private boolean isPositionValid ( int row, int col){
-                return row >= 1 && row <= 8 && col >= 1 && col <= 8;
-            }
+
         }
         return List.of();
     }
